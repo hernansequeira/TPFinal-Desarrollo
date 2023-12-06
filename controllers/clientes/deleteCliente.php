acc136f7969d69e17ecb905e28a0224226fbf5bb
hernansequeira <105883230+hernansequeira@users.noreply.github.com>
2023-12-06 17:25:21 -0300

    update-deleteCliente

--- controllers/clientes/deleteCliente.php
+++ controllers/clientes/deleteCliente.php
@@ -5,30 +5,30 @@ $id = $_GET['idcliente'];
 require_once '../../models/Cliente.php';
 
 try {
-    // Validar el parámetro idcliente
+
     if (!isset($_GET['idcliente']) || !is_numeric($_GET['idcliente'])) {
         throw new Exception("ID de cliente no válido");
     }
 
     $id = $_GET['idcliente'];
 
-    // Crear una instancia de la clase Cliente
+
     $conexion = new Conexion();
     $conexion->conectar();
 
-    // Verificar la conexión a la base de datos
+
     if (!$conexion->con) {
         throw new Exception("Error en la conexión a la base de datos: " . mysqli_connect_error());
     }
 
-    // Crear una instancia de la clase Cliente
+
     $cliente = new Cliente();
 
-    // Validar la existencia del cliente
+
     if (!$cliente->get($id)) {
         echo "El cliente con ID $id no existe.";
     } else {
-        // Intentar eliminar el cliente
+
         $result = $cliente->delete($id);
 
         if ($result) {
