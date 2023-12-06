bfe6fb715fe34ecb1d7f0579c93f4a40988537a8
hernansequeira <105883230+hernansequeira@users.noreply.github.com>
2023-12-06 18:50:07 -0300

    update-all-create

--- views/clientes/editarCliente.view.php
+++ views/clientes/editarCliente.view.php
@@ -12,10 +12,7 @@ require_once '../../layouts/headerview.php';
                     <h3 class="mb-0">Clientes</h3>
                 </div>
                 <div class="col-sm-6">
-                    <ol class="breadcrumb float-sm-end">
-                        <li class="breadcrumb-item"><a href="#">Home</a></li>
-                        <li class="breadcrumb-item active" aria-current="page">Dashboard</li>
-                    </ol>
+
                 </div>
             </div>
         </div>
